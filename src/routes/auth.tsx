9dfb3c2cf21068becc9d436a0428129e9c9e0098
jinwook-song <wlsdnr129@naver.com>
2021-12-17 16:42:33 +0900

    Social Login

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -3,6 +3,9 @@ import { EMAIL_VALIDATION_CHECK } from "types.d";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 import { authService } from "../firebase";
 import { useState } from "react";
@@ -17,10 +20,12 @@ enum AuthErrorType {
   "USER_NOT_FOUND" = "auth/user-not-found",
   "WRONG_PASSWORD" = "auth/wrong-password",
   "TOO_MANY_REQUESTS" = "auth/too-many-requests",
+  "EXIST_DIFFERENT_CREDENTIAL" = "auth/account-exists-with-different-credential",
 }
 
 type AuthErrors = {
   message: string;
+  code: string;
 };
 
 function Auth() {
@@ -35,7 +40,7 @@ function Auth() {
     mode: "onChange",
   });
 
-  const onSubmit = async () => {
+  const onEmailLogin = async () => {
     const { email, password } = getValues();
     if (newAccount) {
       // Create Account
@@ -48,16 +53,14 @@ function Auth() {
         setAuthError(null);
         console.log("created and login", user);
       } catch (error) {
-        switch (true) {
+        switch ((error as AuthErrors).code) {
           // 사용중인 계정
-          case (error as AuthErrors).message //
-            .includes(AuthErrorType.USER_EXIST):
+          case AuthErrorType.USER_EXIST:
             setAuthError("Account already in use");
             setNewAccount(false);
             break;
           // 너무 많은 요청 발생
-          case (error as AuthErrors).message //
-            .includes(AuthErrorType.TOO_MANY_REQUESTS):
+          case AuthErrorType.TOO_MANY_REQUESTS:
             setAuthError("Too many requests were made. Please use it later");
             break;
           // 예상하지 못한 에러
@@ -76,21 +79,18 @@ function Auth() {
         setAuthError(null);
         console.log("login", user);
       } catch (error) {
-        switch (true) {
+        switch ((error as AuthErrors).code) {
           // 계정이 없는 경우
-          case (error as AuthErrors).message //
-            .includes(AuthErrorType.USER_NOT_FOUND):
+          case AuthErrorType.USER_NOT_FOUND:
             setAuthError("Please create an account first");
             setNewAccount(true);
             break;
           // 비밀번호 틀린 경우
-          case (error as AuthErrors).message //
-            .includes(AuthErrorType.WRONG_PASSWORD):
+          case AuthErrorType.WRONG_PASSWORD:
             setAuthError("Wrong Password");
             break;
           // 너무 많은 요청 발생
-          case (error as AuthErrors).message //
-            .includes(AuthErrorType.TOO_MANY_REQUESTS):
+          case AuthErrorType.TOO_MANY_REQUESTS:
             setAuthError("Too many requests were made. Please use it later");
             break;
           // 예상하지 못한 에러
@@ -101,39 +101,78 @@ function Auth() {
     }
   };
 
+  const onSocialLogin = async ({
+    currentTarget: { name },
+  }: React.MouseEvent<HTMLButtonElement>) => {
+    let provider, result, credential;
+    try {
+      switch (name) {
+        case "google":
+          provider = new GoogleAuthProvider();
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          result = await signInWithPopup(authService, provider);
+          credential = GoogleAuthProvider.credentialFromResult(result);
+          break;
+        case "github":
+          provider = new GithubAuthProvider();
+          result = await signInWithPopup(authService, provider);
+          credential = GithubAuthProvider.credentialFromResult(result);
+          break;
+      }
+      console.log(name, credential);
+    } catch (error) {
+      switch ((error as AuthErrors).code) {
+        // Email이 겹치는 경우
+        case AuthErrorType.EXIST_DIFFERENT_CREDENTIAL:
+          setAuthError(
+            "This email is being used by another account. Please log in using another method"
+          );
+          break;
+      }
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register("email", {
-          required: "Email is required.",
-          pattern: {
-            value: EMAIL_VALIDATION_CHECK,
-            message: "Please enter a valid email.",
-          },
-        })}
-        type="email"
-        placeholder="Email"
-        className="input"
-      />
-      {errors.email?.message && <div>{errors.email.message}</div>}
-      <input
-        {...register("password", {
-          required: "Password is required.",
-          minLength: {
-            value: 4,
-            message: "Password should contain more than 4 chars.",
-          },
-        })}
-        type="password"
-        placeholder="Password"
-        className="input"
-      />
-      {errors.password?.message && <div>{errors.password.message}</div>}
-      <button disabled={isValid ? false : true}>
-        {newAccount ? "Create Account" : "Sign In"}
+    <div>
+      <form onSubmit={handleSubmit(onEmailLogin)}>
+        <input
+          {...register("email", {
+            required: "Email is required.",
+            pattern: {
+              value: EMAIL_VALIDATION_CHECK,
+              message: "Please enter a valid email.",
+            },
+          })}
+          type="email"
+          placeholder="Email"
+          className="input"
+        />
+        {errors.email?.message && <div>{errors.email.message}</div>}
+        <input
+          {...register("password", {
+            required: "Password is required.",
+            minLength: {
+              value: 4,
+              message: "Password should contain more than 4 chars.",
+            },
+          })}
+          type="password"
+          placeholder="Password"
+          className="input"
+        />
+        {errors.password?.message && <div>{errors.password.message}</div>}
+        <button disabled={isValid ? false : true}>
+          {newAccount ? "Create Account" : "Sign In"}
+        </button>
+        {authError && <span>{authError}</span>}
+      </form>
+      <button name="google" onClick={onSocialLogin}>
+        Continue with Google
       </button>
-      {authError && <span>{authError}</span>}
-    </form>
+      <button name="github" onClick={onSocialLogin}>
+        Continue with Github
+      </button>
+    </div>
   );
 }
 
