167f8555fbb906e84e347d07771220f6ba0a4fdf
jinwook-song <wlsdnr129@naver.com>
2021-12-29 16:07:12 +0900

    환경변수

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,14 +1,14 @@
-import { useForm } from "react-hook-form";
-import { EMAIL_VALIDATION_CHECK } from "../types.d";
+import { useForm } from 'react-hook-form';
+import { EMAIL_VALIDATION_CHECK } from '../types.d';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
   GithubAuthProvider,
   signInWithPopup,
-} from "firebase/auth";
-import { authService } from "../firebase";
-import { useState } from "react";
+} from 'firebase/auth';
+import { authService } from '../firebase';
+import { useState } from 'react';
 
 type AuthInputs = {
   email: string;
@@ -16,11 +16,11 @@ type AuthInputs = {
 };
 
 enum AuthErrorType {
-  "USER_EXIST" = "auth/email-already-in-use",
-  "USER_NOT_FOUND" = "auth/user-not-found",
-  "WRONG_PASSWORD" = "auth/wrong-password",
-  "TOO_MANY_REQUESTS" = "auth/too-many-requests",
-  "EXIST_DIFFERENT_CREDENTIAL" = "auth/account-exists-with-different-credential",
+  'USER_EXIST' = 'auth/email-already-in-use',
+  'USER_NOT_FOUND' = 'auth/user-not-found',
+  'WRONG_PASSWORD' = 'auth/wrong-password',
+  'TOO_MANY_REQUESTS' = 'auth/too-many-requests',
+  'EXIST_DIFFERENT_CREDENTIAL' = 'auth/account-exists-with-different-credential',
 }
 
 type AuthErrors = {
@@ -37,7 +37,7 @@ function Auth() {
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<AuthInputs>({
-    mode: "onChange",
+    mode: 'onChange',
   });
 
   const onEmailLogin = async () => {
@@ -51,21 +51,21 @@ function Auth() {
           password
         );
         setAuthError(null);
-        console.log("created and login", user);
+        console.log('created and login', user);
       } catch (error) {
         switch ((error as AuthErrors).code) {
           // 사용중인 계정
           case AuthErrorType.USER_EXIST:
-            setAuthError("Account already in use");
+            setAuthError('Account already in use');
             setNewAccount(false);
             break;
           // 너무 많은 요청 발생
           case AuthErrorType.TOO_MANY_REQUESTS:
-            setAuthError("Too many requests were made. Please use it later");
+            setAuthError('Too many requests were made. Please use it later');
             break;
           // 예상하지 못한 에러
           default:
-            console.log("Unexpected create account", error);
+            console.log('Unexpected create account', error);
         }
       }
     } else {
@@ -77,25 +77,25 @@ function Auth() {
           password
         );
         setAuthError(null);
-        console.log("login", user);
+        console.log('login', user);
       } catch (error) {
         switch ((error as AuthErrors).code) {
           // 계정이 없는 경우
           case AuthErrorType.USER_NOT_FOUND:
-            setAuthError("Please create an account first");
+            setAuthError('Please create an account first');
             setNewAccount(true);
             break;
           // 비밀번호 틀린 경우
           case AuthErrorType.WRONG_PASSWORD:
-            setAuthError("Wrong Password");
+            setAuthError('Wrong Password');
             break;
           // 너무 많은 요청 발생
           case AuthErrorType.TOO_MANY_REQUESTS:
-            setAuthError("Too many requests were made. Please use it later");
+            setAuthError('Too many requests were made. Please use it later');
             break;
           // 예상하지 못한 에러
           default:
-            console.log("Unexpected create account", error);
+            console.log('Unexpected create account', error);
         }
       }
     }
@@ -107,13 +107,13 @@ function Auth() {
     let provider, result, credential;
     try {
       switch (name) {
-        case "google":
+        case 'google':
           provider = new GoogleAuthProvider();
           // This gives you a Google Access Token. You can use it to access the Google API.
           result = await signInWithPopup(authService, provider);
           credential = GoogleAuthProvider.credentialFromResult(result);
           break;
-        case "github":
+        case 'github':
           provider = new GithubAuthProvider();
           result = await signInWithPopup(authService, provider);
           credential = GithubAuthProvider.credentialFromResult(result);
@@ -125,7 +125,7 @@ function Auth() {
         // Email이 겹치는 경우
         case AuthErrorType.EXIST_DIFFERENT_CREDENTIAL:
           setAuthError(
-            "This email is being used by another account. Please log in using another method"
+            'This email is being used by another account. Please log in using another method'
           );
           break;
       }
@@ -136,40 +136,40 @@ function Auth() {
     <div>
       <form onSubmit={handleSubmit(onEmailLogin)}>
         <input
-          {...register("email", {
-            required: "Email is required.",
+          {...register('email', {
+            required: 'Email is required.',
             pattern: {
               value: EMAIL_VALIDATION_CHECK,
-              message: "Please enter a valid email.",
+              message: 'Please enter a valid email.',
             },
           })}
-          type="email"
-          placeholder="Email"
-          className="input"
+          type='email'
+          placeholder='Email'
+          className='input'
         />
         {errors.email?.message && <div>{errors.email.message}</div>}
         <input
-          {...register("password", {
-            required: "Password is required.",
+          {...register('password', {
+            required: 'Password is required.',
             minLength: {
               value: 4,
-              message: "Password should contain more than 4 chars.",
+              message: 'Password should contain more than 4 chars.',
             },
           })}
-          type="password"
-          placeholder="Password"
-          className="input"
+          type='password'
+          placeholder='Password'
+          className='input'
         />
         {errors.password?.message && <div>{errors.password.message}</div>}
         <button disabled={isValid ? false : true}>
-          {newAccount ? "Create Account" : "Sign In"}
+          {newAccount ? 'Create Account' : 'Sign In'}
         </button>
         {authError && <span>{authError}</span>}
       </form>
-      <button name="google" onClick={onSocialLogin}>
+      <button name='google' onClick={onSocialLogin}>
         Continue with Google
       </button>
-      <button name="github" onClick={onSocialLogin}>
+      <button name='github' onClick={onSocialLogin}>
         Continue with Github
       </button>
     </div>
